234c34008ba24ad6f3c0da551d4f007d94b12a84
Rechimon, Andrés <andres.r1995@hotmail.com>
2024-08-07 23:53:13 -0300

    Initial Commit

---
+++ shadaloocri-uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shadaloocri-uikit
+//
+//  Created by Andres Rechimon on 07/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
