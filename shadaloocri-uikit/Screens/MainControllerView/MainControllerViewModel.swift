52c865c78f80ced036f70a90f5e4c02b860ac5f7
Rechimon, Andrés. <andres.r1995@hotmail.com>
2024-08-10 18:16:49 -0300

    viewmodel refactor

---
+++ shadaloocri-uikit/Screens/MainControllerView/MainControllerViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  MainControllerViewModel.swift
+//  shadaloocri-uikit
+//
+//  Created by Andres Rechimon on 10/08/2024.
+//
+
+import Foundation
+
+class MainControllerViewModel {
+    let titles: [SFTitleCell] = [
+        SFTitleCell(image: ConstantImage.ssf2tLogo, name: "Super Street fighter 2 Turbo"),
+        SFTitleCell(image: ConstantImage.sf3tiLogo, name: "Street Fighter 3: Third Impact"),
+        SFTitleCell(image: ConstantImage.usfivLogo, name: "Ultra Street Fighter 4"),
+    ]
+}
--- shadaloocri-uikit/Screens/MainControllerView/MainViewController.swift
+++ shadaloocri-uikit/Screens/MainControllerView/MainViewController.swift
@@ -8,12 +8,7 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    
-    private let titles: [SFTitleCell] = [
-        SFTitleCell(image: ConstantImage.ssf2tLogo, name: "Super Street fighter 2 Turbo"),
-        SFTitleCell(image: ConstantImage.sf3tiLogo, name: "Street Fighter 3: Third Impact"),
-        SFTitleCell(image: ConstantImage.usfivLogo, name: "Ultra Street Fighter 4"),
-    ]
+    private var viewModel: MainControllerViewModel = MainControllerViewModel()
     
     private lazy var layout: UICollectionViewLayout = {
             let layout = UICollectionViewFlowLayout()
@@ -38,7 +33,6 @@ class MainViewController: UIViewController {
         return label
     }()
     
-    //collection
     private lazy var sfTitlesCarouselCollectionView: UICollectionView = {
         let collection = UICollectionView(frame: .zero, collectionViewLayout: self.layout)
         collection.backgroundColor = .clear
@@ -56,19 +50,16 @@ class MainViewController: UIViewController {
         return collection
     }()
     
-    //PAGE CONTROL
-        private lazy var cardPageControl: UIPageControl = {
-            let control: UIPageControl = .init()
-            control.translatesAutoresizingMaskIntoConstraints = false
-            control.pageIndicatorTintColor = .gray
-            control.currentPageIndicatorTintColor = .heavyGold
-            control.isUserInteractionEnabled = false
-            control.numberOfPages = titles.count
-            
-            return control
-        }()
-        //PAGE CONTROL
-    //collection
+    private lazy var cardPageControl: UIPageControl = {
+        let control: UIPageControl = .init()
+        control.translatesAutoresizingMaskIntoConstraints = false
+        control.pageIndicatorTintColor = .gray
+        control.currentPageIndicatorTintColor = .heavyGold
+        control.isUserInteractionEnabled = false
+        control.numberOfPages = viewModel.titles.count
+        
+        return control
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -108,13 +99,13 @@ extension MainViewController: UICollectionViewDataSource, UICollectionViewDelega
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
             guard let cell: SFTitlesCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "SFTitlesCollectionViewCell", for: indexPath) as? SFTitlesCollectionViewCell else { fatalError() }
-            cell.setup(item: titles[indexPath.row])
+        cell.setup(item: viewModel.titles[indexPath.row])
             
             return cell
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return titles.count
+        return viewModel.titles.count
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
