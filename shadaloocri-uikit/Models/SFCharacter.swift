2e985d28d0bee03490bb135e953c24a72324134e
Rechimon, Andrés. <andres.r1995@hotmail.com>
2024-08-20 22:43:05 -0300

    ssf2t akuma + characters bio

--- shadaloocri-uikit/Models/SFCharacter.swift
+++ shadaloocri-uikit/Models/SFCharacter.swift
@@ -11,4 +11,9 @@ struct SFCharacter {
     public let title: SFEntry
     public let avatar: String
     public let name: String
+    public let bio: String
+    public let country: String
+    public let specialMoves: [String]
+    public let debutYear: Int
+    public let debutGame: String
 }
